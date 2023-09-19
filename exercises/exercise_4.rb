93b74277be9823533eba838e375fe0b72c67db6a
JacquelineMG <j.gilchrist@outlook.com>
2023-09-19 14:59:44 -0700

    looping through stores

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,24 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+store4 = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+store5 = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+store6 = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each do |men|
+  puts "Mens Store Name:#{men.name}, Annual Revenue: #{men.annual_revenue}"
+end
+
+puts "---------------------------------"
+@womens_stores = Store.where(womens_apparel: true && "annual_revenue < 1000000")
+
+@womens_stores.each do |women|
+  puts "Womens Store Name:#{women.name}, Annual Revenue: #{women.annual_revenue}"
+end
+
+
+
+
